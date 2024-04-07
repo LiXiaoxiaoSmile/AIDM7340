d96cdc23a0414f916b2f579a3fb39a332c73ea1d
LiXiaoxiaoSmile <xiaoxiaosmile1@gmail.com>
2024-04-08 02:20:37 +0800

    Update (but don't know how to say what i update :) )

--- predict.py
+++ predict.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 
 # Load a pretrained YOLOv8n model
-model = YOLO(r'C:\Users\LQA\Desktop\class\Lxx\runs\detect\v8mtest3\weights\best.pt')
+model = YOLO(r'/Users/appmacbook/Desktop/AIDM7340/yolov8n.pt')
 
 # Define path to video file
-source = r'C:\Users\LQA\Desktop\class\Lxx\sample\474288614-1-16.mp4'
+source = r'/Users/appmacbook/Desktop/AIDM7340/sample/474288614-1-208.mp4'
 
 # Run inference on the source
-results = model(source, stream=True, show=True, save=True, save_conf=True)  # generator of Results objects
+results = model(source, stream=True, show=True, save=True, save_conf=True, device='mps')  # generator of Results objects
 
 for r in results:
     boxes = r.boxes  # Boxes object for bbox outputs
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 # model = YOLO('yolov8n.yaml').load('yolov8n.pt')
 
-results = model.train(data=r'C:\Users\LQA\Desktop\class\Lxx\dataset\dataset.yaml', epochs=100, imgsz=640, device=0,
+results = model.train(data=r'/Users/appmacbook/Desktop/AIDM7340/datasets/trainconfig.yaml', epochs=100, imgsz=640, device='mps',
                       name='v8mtest', workers=0)
