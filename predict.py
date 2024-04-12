a52372a5c1533361d5bb029a8fe4803a78c226a3
LiXiaoxiaoSmile <xiaoxiaosmile1@gmail.com>
2024-04-12 21:36:40 +0800

    [Update - trainingconfig.yaml] Create a new trainingconfig.yaml for Colab Use.

--- predict.py
+++ predict.py
@@ -1,15 +1,40 @@
 from ultralytics import YOLO
+from PIL import Image
+import torch
 
 # Load a pretrained YOLOv8n model
 model = YOLO(r'/Users/appmacbook/Desktop/AIDM7340/yolov8n.pt')
 
 # Define path to video file
-source = r'/Users/appmacbook/Desktop/AIDM7340/sample/474288614-1-208.mp4'
+source = r'sample/474288614-1-208.mp4'
+
+if torch.backends.mps.is_built():
+    print("Apple Device GPU Detect, Using MPS for Speed Up.")
+    device = 'mps'
+elif torch.backends.cuda.is_built():
+    print("CUDA Device Detect, Using CUDA for Speed Up.")
+    device = 0
+else:
+    print("No Speed Up Device Detect, Using CPU. Caution: This may cause the Speed SlowDown.")
+    device = 'cpu'
+
 
 # Run inference on the source
-results = model(source, stream=True, show=True, save=True, save_conf=True, device='mps')  # generator of Results objects
+results = model(source, stream=True, show=True, save=True, save_conf=True, device=device)  # generator of Results objects
 
 for r in results:
     boxes = r.boxes  # Boxes object for bbox outputs
     masks = r.masks  # Masks object for segment masks outputs
     probs = r.probs  # Class probabilities for classification outputs
+
+# Visualize the results
+for i, r in enumerate(results):
+    # Plot results image
+    im_bgr = r.plot()  # BGR-order numpy array
+    im_rgb = Image.fromarray(im_bgr[..., ::-1])  # RGB-order PIL image
+
+    # Show results to screen (in supported environments)
+    r.show()
+
+    # Save results to disk
+    r.save(filename=f'results{i}.jpg')
--- train.py
+++ train.py
@@ -1,7 +1,31 @@
 from ultralytics import YOLO
+from datetime import datetime
+import torch
 
-model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
-# model = YOLO('yolov8n.yaml').load('yolov8n.pt')
+model = YOLO('yolov8n.pt')
 
-results = model.train(data=r'/Users/appmacbook/Desktop/AIDM7340/datasets/trainconfig.yaml', epochs=100, imgsz=640, device='mps',
-                      name='v8mtest', workers=0)
+local_train_config_filepath = r'./datasets/trainconfig-local.yaml'
+colab_train_config_filepath = r'/content/AIDM7340/datasets/trainconfig-colab.yaml'
+
+epoch = 100
+imgsize = 640
+
+result_name = f'AIDM7340-GroupProject-{datetime.now()}'
+
+if torch.backends.mps.is_built():
+    print("Apple Device GPU Detect, Using MPS for Speed Up.")
+    device = 'mps'
+elif torch.backends.cuda.is_built():
+    print("CUDA Device Detect, Using CUDA for Speed Up.")
+    device = 0
+else:
+    print("No Speed Up Device Detect, Using CPU. Caution: This may cause the Speed SlowDown.")
+    device = 'cpu'
+
+
+try:
+    results = model.train(data=local_train_config_filepath, epochs=epoch, imgsz=imgsize, device=device, name=result_name, workers=0)
+except:
+    print("Fail to load the local train config file, using Colab Version")
+    results = model.train(data=colab_train_config_filepath, epochs=epoch, imgsz=imgsize, device=device, name=result_name, workers=0)
+    
